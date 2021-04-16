67a6ad5ad6ceb9f2f25a3bd60e8a0bb8e86b3626
Susideur-YT <56844734+Susideur@users.noreply.github.com>
2021-04-16 16:37:21 +0200

    add all numworks key

--- __init__.py
+++ __init__.py
@@ -1,53 +1,63 @@
-#This is the Ion module of Numworks.
+"""
+This is the adaptation of Ion module of Numworks.
+Please don't use keyboard and this module at the same time.
+"""
+### v All keys of Numworks v ###
+KEY_LEFT="left"
+KEY_UP="up"
+KEY_DOWN="down"
+KEY_RIGHT="right"
+KEY_OK="return"
+KEY_BACK="del"
+KEY_HOME="home"
+KEY_ONOFF="end"
+KEY_SHIFT="shift"
+KEY_ALPHA="ctrl"
+KEY_XNT=":"
+KEY_VAR=";"
+KEY_TOOLBOX="\""
+KEY_BACKSPACE="backspace"
+KEY_EXP="["
+KEY_LN="]"
+KEY_LOG="{"
+KEY_IMAGINARY="}"
+KEY_COMMA=","
+KEY_POWER="^"
+KEY_SINE="s"
+KEY_COSINE="c"
+KEY_TANGENT="t"
+KEY_PI="="
+KEY_SQRT="<"
+KEY_SQUARE="²"
+KEY_SEVEN="7"
+KEY_EIGHT="8"
+KEY_NINE="9"
+KEY_LEFTPARENTHESIS="("
+KEY_RIGHTPARENTHESIS=")"
+KEY_FOUR="4"
+KEY_FIVE="5"
+KEY_SIX="6"
+KEY_MULTIPLICATION="*"
+KEY_DIVISION="/"
+KEY_ONE="1"
+KEY_TWO="2"
+KEY_THREE="3"
+KEY_PLUS="+"
+KEY_MINUS="-"
+KEY_ZERO="0"
+KEY_DOT="."
+KEY_EE="insert"
+KEY_ANS="@"
+KEY_EXE="enter"
+### ^ All keys of Numworks ^ ###
 
-from keyboard import *
-
-KEY_LEFT=0
-KEY_UP=1
-KEY_DOWN=2
-KEY_RIGHT=3
-KEY_OK=4
-KEY_BACK=5
-KEY_HOME=6
-KEY_ONOFF=7
-KEY_SHIFT=8
-KEY_ALPHA=9
-KEY_XNT=10
-KEY_VAR=11
-KEY_TOOLBOX=12
-KEY_BACKSPACE=13
-KEY_LN=14
-KEY_LOG=15
-KEY_IMAGINARY=16
-KEY_COMMA=17
-KEY_POWER=18
-KEY_SINE=19
-KEY_COSINE=20
-KEY_TANGENT=21
-KEY_PI=22
-KEY_SQRT=23
-KEY_SQUARE=24
-KEY_SEVEN=25
-KEY_EIGHT=26
-KEY_NINE=27
-KEY_LEFTPARENTHESIS=28
-KEY_RIGHTPARENTHESIS=29
-KEY_FOUR=30
-KEY_FIVE=31
-KEY_SIX=32
-KEY_MULTIPLICATION=33
-KEY_DIVISION=34
-KEY_ONE=35
-KEY_TWO=36
-KEY_THREE=37
-KEY_PLUS=38
-KEY_MINUS=39
-KEY_DOT=40
-KEY_EE=41
-KEY_ANS=42
-KEY_EXE=43
-
-KEYS_=[4,8,2,6,"\n","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","\n"]
-
+from keyboard import is_pressed
 def keydown(k):
-  return is_pressed(KEYS_[k])
+  return is_pressed(k)
+
+"""
+# v It's just for testing my code.
+while True:
+  if keydown(KEY_HOME):
+    break
+"""
